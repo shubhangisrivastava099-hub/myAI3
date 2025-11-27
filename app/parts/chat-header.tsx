86984e68c92daf2f435b91c85aba848c893a372d
Nikita Kulkarni <79144639+nikita-n-k@users.noreply.github.com>
2025-11-27 16:51:46 +0530

    Update chat-header.tsx

--- app/parts/chat-header.tsx
+++ app/parts/chat-header.tsx
@@ -1,17 +1,20 @@
+import React from "react";
 import { cn } from "@/lib/utils";
 
-export function ChatHeaderBlock({ children, className }: { children?: React.ReactNode, className?: string }) {
-    return (
-        <div className={cn("gap-2 flex flex-1", className)}>
-            {children}
-        </div>
-    )
+export function ChatHeaderBlock({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return <div className={cn("gap-2 flex flex-1", className)}>{children}</div>;
 }
 
 export function ChatHeader({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="w-full flex py-5 px-5 bg-linear-to-b from-background to-transparent">
-            {children}
-        </div>
-    )
+  return (
+    <div className="w-full flex items-center gap-2 px-5 py-3">
+      {children}
+    </div>
+  );
 }
